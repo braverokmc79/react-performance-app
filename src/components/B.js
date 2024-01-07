9c3787f1df4bf56ebb0f45e7a6e1b8f4face0519
braverokmc79 <braverokmc79@gmail.com>
2024-01-07 15:01:16 +0900

    08.useCallback 을 이용한 함수최적화

--- src/components/B.js
+++ src/components/B.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 
 const Message=React.memo(({message})=>{
     return <p>{message}</p>
 });
 
-const ListItem=React.memo(({post})=>{
+const ListItem=React.memo(({post, testFunction})=>{
+    const onKey=()=>{
+        testFunction("hello world");
+    }
+
     return (
-        <li key={post.id}>
+        <li key={post.id} onClick={onKey}>
                 <p>{post.title}</p>
         </li>
     )   
 });
 
-const List=React.memo(({posts})=>{
+const List=React.memo(({posts, testFunction})=>{
+    console.log("List Components rendered");
+
+ 
+
     return (
         <ul>
            {posts.map(post=>(           
-               <ListItem  key={post.id}  post={post} />              
+               <ListItem  key={post.id}  post={post}  testFunction={testFunction} />              
            ))}
         </ul>
     )
@@ -25,11 +33,18 @@ const List=React.memo(({posts})=>{
 
 const B = ({message,posts}) => {
 
+  console.log("B Components rendered");
+
+  const testFunction=useCallback((mes) => {   
+        console.log("message: ",mes);
+  },[]);
+
+
   return (
     <div>
         <h1>B Components</h1>
         <Message message={message} />
-        <List posts={posts} />
+        <List posts={posts} testFunction={testFunction}/>
     </div>
   )
 }
