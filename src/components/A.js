4f2c2cf312a25198172e108e7df21de06ff306dc
braverokmc79 <braverokmc79@gmail.com>
2024-01-07 13:31:06 +0900

    first commit

---
+++ src/components/A.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+const A = ({message, posts}) => {
+     
+    return (
+    <div>
+        <h1>A Components</h1>
+        <p>{message}</p>
+        <ul>
+            {posts.map(post => 
+                {
+                  return(
+                       <li key={post.id}>
+                            <p>{post.title}</p>
+                        </li>
+                  )
+                }
+            )}
+        </ul>
+    </div>
+  )
+}
+
+export default A
